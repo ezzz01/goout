d8dc70dd7e2993f9f428f39fe5b9ad21bd83cbbe
ezhux1 <ezhux001@yahoo.com>
2009-02-15 20:48:09 +0000

    nevykes testas, kazkas blogai is ankstesniu testu

--- test/functional/site_controller_test.rb
+++ test/functional/site_controller_test.rb
@@ -2,7 +2,22 @@ require File.dirname(__FILE__) + '/../test_helper'
 
 class SiteControllerTest < ActionController::TestCase
   # Replace this with your real tests.
-  def test_truth
-    assert true
+  def test_index
+    get :index
+    assert_response :success
+    assert_template "index"
   end
+
+  def test_about
+    get :about
+    assert_response :success
+    assert_template "about"
+  end
+
+  def test_help
+    get :help
+    assert_response :help
+    assert_template "help"
+  end
+
 end
