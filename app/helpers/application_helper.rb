0719a2d1d95ef916e02edc05122f85fc3974bd31
ezhux1 <ezhux001@yahoo.com>
2009-08-07 13:16:29 +0000

    patasyta klaida 'or_else'

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,7 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  require 'string'
+
   # create link for navigation
   def nav_link(text, controller, action="index")
     link_to_unless_current text, :controller => controller, :action => action
