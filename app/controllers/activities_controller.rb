e3ffa1fb1f4628b06e4d5fb682b2b5c718466471
ezhux <ezhux001@yahoo.com>
2010-01-20 21:51:16 +0000

    beveik sutvarkyta Activities forma.

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -32,13 +32,22 @@ class ActivitiesController < ApplicationController
   end
 
   def update_organizations
-    country = Country.find_by_id(params[:country_id], :include => :organizations, :order => 'organizations.title', :conditions => [ "organizations.pending = 0 OR organizations.added_by = ?", session[:user_id] ])
+    country = Country.find_by_id(params[:country_id], :include => :organizations, :order => 'organizations.type, organizations.title', :conditions => [ "organizations.pending = 0 OR organizations.added_by = ?", session[:user_id] ])
+    organizations = {}
+    country.organizations.each do |org|
+        mytest = {org.title => org.id}
+        organizations[org.type] = mytest
+    end
+
+  #  organizations = [ ['North America', [['United States','US'],'Canada']], ['Europe', ['Denmark','Germany','France']] ]
+
 	render :update do |page|
         if country.blank?
-            page.replace_html 'organization', :partial => 'organizations', :locals => {:id => params[:country_id] }, :object => nil
+            page.replace_html 'organization', :partial => 'organizations', :locals => {:id => params[:country_id], :organizations => organizations}
         else 
-            page.replace_html 'organization', :partial => 'organizations', :locals => {:id => params[:country_id] }, :object => country.organizations
+            page.replace_html 'organization', :partial => 'organizations', :locals => {:id => params[:country_id], :organizations => organizations}
         end
+        page[:activity_organization_id].set_style :width => "400px"
         page << "initialize();" 
 	end
   end
@@ -58,12 +67,27 @@ class ActivitiesController < ApplicationController
 
     def update_fields
 	    render :update do |page|
-            if (params[:activity_type] == "exchange_study")
+            if (params[:activity_type] == "full_study")
+                page.hide "exchange_program"
+                page.hide "activity_area"
+                page.show "subject_area"
+                page.show "study_program"
+            elsif (params[:activity_type] == "exchange_study")
                 exchange_programs = ExchangeProgram.find(:all, :order => :title)
                 page.replace_html 'exchange_program', :partial => 'exchange_programs', :locals => {}, :object => exchange_programs 
+                page[:activity_exchange_program_id].set_style :width => "400px"
+                page.show "exchange_program"
+                page.hide "activity_area"
+                page.show "subject_area"
+                page.show "study_program"
             elsif (params[:activity_type] == "internship")
                 activity_areas = ActivityArea.find(:all, :order => :title)
                 page.replace_html 'activity_area', :partial => 'activity_areas', :locals => {}, :object => activity_areas 
+                page[:activity_activity_area_id].set_style :width => "400px"
+                page.hide "exchange_program"
+                page.show "activity_area"
+                page.hide "subject_area"
+                page.hide "study_program"
             end
             page << "initialize();" 
         end
